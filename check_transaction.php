ca1daa251973e6fabc289f3c1b6aaac679ad667d
Mohamed elabady <me@xpay.app>
2021-07-06 17:53:06 +0200

    otaka changes - ifreame url

--- check_transaction.php
+++ check_transaction.php
@@ -9,7 +9,7 @@ $community_id  = $_REQUEST["community_id"];
 $order_id  = $_REQUEST["order_id"];
 $wc_settings = new WC_Gateway_Xpay;
 
-$url = $wc_settings->get_option("iframe_base_url"). "/api/communities/".$community_id."/transactions/".$uuid."/";
+$url = $wc_settings->get_option("iframe_base_url"). "/api/v1/communities/".$community_id."/transactions/".$uuid."/";
 $resp = httpGet($url, $wc_settings->get_option("payment_api_key"));
 $resp = json_decode($resp, TRUE);
 $order = wc_get_order( $order_id );
