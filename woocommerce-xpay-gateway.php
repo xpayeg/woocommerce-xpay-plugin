762e4dd7defa4d2c489fb1264aabf1e22b7b20e1
melabady <me@xpay.app>
2023-02-18 02:45:11 +0200

    update bootstrap links

--- woocommerce-xpay-gateway.php
+++ woocommerce-xpay-gateway.php
@@ -190,9 +190,9 @@ if(!function_exists("generate_payment_modal")) {
     // jQuery code start below
     ?>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 	<script>
 		jQuery( function($){
 			$('#myModal').modal({
