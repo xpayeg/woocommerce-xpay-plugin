4c01a2c54c654741ae1adcf20f1aa523ba1ba2fc
melabady <me@xpay.app>
2023-11-23 12:42:25 +0200

    apply multi changes-path is calculated dynamic now and fix bug when modal cloed it cannot open

--- check_transaction.php
+++ check_transaction.php
@@ -9,9 +9,8 @@ $uuid = $_REQUEST["trn_uuid"];
 $community_id  = $_REQUEST["community_id"];
 $order_id  = $_REQUEST["order_id"];
 $wc_settings = new WC_Gateway_Xpay;
-$debug = $wc_settings->get_option("debug");
 $url = $wc_settings->get_option("iframe_base_url"). "/api/v1/communities/".$community_id."/transactions/".$uuid."/";
-$resp = httpGet($url, $wc_settings->get_option("payment_api_key"), $debug);
+$resp = httpGet($url, $wc_settings->get_option("payment_api_key"));
 $resp = json_decode($resp, TRUE);
 $order = wc_get_order( $order_id );
 if ($resp["data"]["status"] == "SUCCESSFUL"){
